608b637714dc839e30b95f1152a26294219ad714
Kamrul Hasan <83187173+Kamrul997@users.noreply.github.com>
2021-09-02 23:03:31 +0600

    Add files via upload

---
+++ project-2/javascript/main.js
@@ -0,0 +1,26 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+
+// <div class="card" >
+//                   <img class="card-img-top" src="https://via.placeholder.com/300x200" alt="Card image cap">
+//                   <div class="card-body">
+//                     <h5 class="card-title">Md. Nasir khan</h5>
+//                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//                     <a href="#" class="btn btn-outline-dark">More Details</a>
+//                   </div>
+//                 </div>
